ede08963c369a0ca22b6154e519f356098831c1c
c-mulge <mulgechannveer@gmail.com>
2024-09-25 20:45:47 +0530

    MyFrame and ex1

---
+++ src/Swing/MyFrame.java
@@ -0,0 +1,12 @@
+package Swing;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    MyFrame(){
+        this.setSize(500,500);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+    }
+}
