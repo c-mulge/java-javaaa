6ae4b21d068b040507fedaab5cc170a8d97eee45
c-mulge <mulgechannveer@gmail.com>
2025-01-05 19:31:48 +0530

    update-1

--- src/MultiThreading/t1.java
+++ src/MultiThreading/t1.java
@@ -1,5 +1,19 @@
 package MultiThreading;
 
 public class t1 {
+    public static void main(String[] args) {
+        // Create an instance of the t2 class which implements Runnable
+        t2 t = new t2();
+
+//        t.start();  //runs when Thread is extended
+
+        // Create a new Thread object, passing the t2 instance to the Thread constructor
+        Thread t1 = new Thread(t);
+
+        // Start the thread, which will call the run method of the t2 instance
+        t1.start();
+
+        // Print the name of the current thread (main thread)
+        System.out.println(Thread.currentThread().getName());
     }
 }
